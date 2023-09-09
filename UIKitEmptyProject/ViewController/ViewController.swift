290537e3a4597208cebc919275683b108bc5ac4b
Ryan Forsyth <forsyth.r@gmail.com>
2023-09-09 18:32:35 +0100

    Initial commit. Remove main.storyboard, instantiate VC programmatically. Reorganize files into groups

---
+++ UIKitEmptyProject/ViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RoundUp
+//
+//  Created by Ryan Forsyth on 2023-09-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
